e274fd250106e3e7f8e31c140d3f55a65c802989
ShreyaChakraborty50 <shreya101716@gmail.com>
2023-09-18 23:13:04 +0600

    authorRouter.js is added

---
+++ routes/authorRouter.js
@@ -0,0 +1,17 @@
+const authenticationController =  require('../controllers/authenticationController.js')
+const authorController = require('../controllers/authorController.js')
+const router = require('express').Router()
+const { missingSignUpFieldMiddleware, missingLogInFieldMiddleware, isEmailAlreadyUsedMiddleware, validationMiddleware } = require('../middleware/validationMiddleware.js');
+
+
+router.post('/signup', missingSignUpFieldMiddleware, validationMiddleware, isEmailAlreadyUsedMiddleware, authenticationController.addAuthor)
+
+router.post('/login', missingLogInFieldMiddleware,validationMiddleware, authenticationController.loginAuthor)
+
+router.get('/', authorController.getAllAuthors)
+
+router.get('/:authorId', authorController.getAuthorById)
+
+
+
+module.exports= router;
