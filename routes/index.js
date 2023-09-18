3b2f1d30d27c7d686ed46b11ca7a6480dfa4b0f2
ShreyaChakraborty50 <shreya101716@gmail.com>
2023-09-18 23:17:50 +0600

    index.js is added in routes

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const authorRouter = require("./authorRouter.js");
+const blogRouter = require("./blogRouter.js")
+
+router.use('/authors', authorRouter);
+router.use('/blogs', blogRouter);
+module.exports = router;
