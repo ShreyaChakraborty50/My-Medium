2b4d6c1a47eeff214d0203b920b65306fbead212
ShreyaChakraborty50 <shreya101716@gmail.com>
2023-09-18 23:51:58 +0600

    authorDTO.js is added in utils

---
+++ utils/authorDTO.js
@@ -0,0 +1,14 @@
+
+
+const authorDTO =  (author)=>{
+    const authorDTO = {
+        authorId : author.authorId,
+        name : author.name,
+        email : author.email
+    }
+    return authorDTO
+}
+
+module.exports = {
+    authorDTO
+}
